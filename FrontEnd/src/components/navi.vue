f34e1d3937f43a1311d4527ab02f274b53554585
James <171250628@smail.nju.edu.cn>
2019-04-09 18:36:21 +0800

    ｕｐｄａｔｅ
    更改了主界面

--- FrontEnd/src/components/navi.vue
+++ FrontEnd/src/components/navi.vue
@@ -1,16 +1,11 @@
 <template>
-  <div id="navi" style="width:100%; padding:0">
+  <div id="navi" style="width:100%;margin:0; padding:0">
     <el-menu :default-active="activeIndex2" class="menu" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
       <el-menu-item index="0"><a href="/"><img src="../assets/logo.png" id="Logo"></a></el-menu-item>
 
-      <el-menu-item index="1"><a href="/" style="text-decoration:none">主页</a></el-menu-item>
-      <el-menu-item index="2"><a href="/Activity" style="text-decoration:none">活动</a></el-menu-item>
-      <el-menu-item index="3"><a href="/Notice" style="text-decoration:none">通知</a></el-menu-item>
-      <el-menu-item index="4"><a href="/Recruit" style="text-decoration:none">招募</a></el-menu-item>
-      <el-menu-item index="5"><a href="/Contact" style="text-decoration:none">联系我们</a></el-menu-item>
-      
-      <el-submenu style="float:right">
-        <template slot="title">个人中心</template>
+      <el-submenu style="float:right">  
+        <template slot="title"><img src="../assets/personal.png" style="width:30px;margin:5px;">个人中心</template>          
+        <p style="color: white;text-align:center">用户名，你好!</p>
         <a href='/userSpace' style="text-decoration:none"><el-menu-item index="2-1">用户模式</el-menu-item></a>
         <a href='/Manage' style="text-decoration:none"><el-menu-item index="2-2">管理模式</el-menu-item></a>
         <el-menu-item v-on:click="logout">退出登录</el-menu-item>
@@ -21,6 +16,13 @@
           <el-menu-item index="2-4-3">选项3</el-menu-item>
         </el-submenu> -->
       </el-submenu>
+
+      <el-menu-item style="float:right"><a href="/Contact" style="text-decoration:none">联系我们</a></el-menu-item>
+      <el-menu-item style="float:right"><a href="/Recruit" style="text-decoration:none">招募</a></el-menu-item>
+      <el-menu-item style="float:right"><a href="/Notice" style="text-decoration:none">通知</a></el-menu-item>
+      <el-menu-item style="float:right"><a href="/Activity" style="text-decoration:none">活动</a></el-menu-item>
+      <el-menu-item style="float:right"><a href="/" style="text-decoration:none">主页</a></el-menu-item>
+      
 
     </el-menu>
   </div>
@@ -44,7 +46,9 @@ export default {
 </script>
 
 <style>
-  .menu{
-    
+  #navi {
+  }
+  #Logo {
+    width: 50px;
   }
 </style>
--- FrontEnd/src/pages/Home.vue
+++ FrontEnd/src/pages/Home.vue
@@ -1,7 +1,69 @@
 <template>
-  <div>
+  <div class="main">
     <navi></navi>
-    <p>主页</p>
+    <div class="announce">
+      <div class="ann_title">
+        <h1 style="margin:0px 0 0 20px;padding:0px;text-align:center;color:#EB7A67">最新通知</h1>
+      </div>
+      <div class="ann_body">
+        <div class="ann_body_list">
+          <el-button @click="resetDateFilter">清除日期过滤器</el-button>
+          <el-button @click="clearFilter">清除所有过滤器</el-button>
+          <a href="/Notice"><h2 style="float:right"> >>>>>More&nbsp&nbsp </h2></a> 
+          <el-table ref="filterTable" :data="tableData" height=240 style="width: 100%;">
+            <el-table-column prop="date" label="日期" sortable width="180" column-key="date" 
+            :filters="[{text: '2016-05-01', value: '2016-05-01'}, {text: '2016-05-02', value: '2016-05-02'}, {text: '2016-05-03', value: '2016-05-03'}, {text: '2016-05-04', value: '2016-05-04'}]"
+            :filter-method="filterHandler">
+            </el-table-column>
+            
+            <el-table-column prop="name" label="姓名" width="180">
+            </el-table-column>
+            
+            <el-table-column prop="address" label="地址" :formatter="formatter">
+            </el-table-column>
+            <el-table-column prop="tag" label="标签" width="100"
+              :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+              :filter-method="filterTag" filter-placement="bottom-end">
+              <template slot-scope="scope">
+                <el-tag :type= "scope.row.tag === '家' ? 'primary' : 'success'"
+                disable-transitions>{{scope.row.tag}}</el-tag>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+      </div>
+    </div>
+
+    <div class="activity">
+      <div class="act_title">
+        <h1 style="margin:0px;text-align:center;color:#EB7A67">最新活动</h1>
+      </div>
+      <div class="act_body">
+        <div class="act_body_list">
+          <a href="/Activity"><h2 style="float:right"> >>>>>More&nbsp&nbsp </h2></a> 
+          <el-table ref="filterTable" :data="tableData" height=240 style="width: 100%;">
+            <el-card class="box-card">
+              <div slot="header" class="clearfix">
+              <span>卡片名称</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+              </div>
+              <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+              </div>
+            </el-card>
+            <!-- <el-table-column prop="tag" label="标签" width="100"
+              :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+              :filter-method="filterTag" filter-placement="bottom-end">
+              <template slot-scope="scope">
+                <el-tag :type= "scope.row.tag === '家' ? 'primary' : 'success'"
+                disable-transitions>{{scope.row.tag}}</el-tag>
+              </template>
+            </el-table-column> -->
+          </el-table>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -12,15 +74,96 @@ export default {
   components: {navi},
   data() {
     return {
-      name: "Name",
-    }
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '家'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          tag: '公司'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          tag: '家'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+          tag: '公司'
+        }]
+      }
   },
   methods: {
-
-  }
+      resetDateFilter() {
+        this.$refs.filterTable.clearFilter('date');
+      },
+      clearFilter() {
+        this.$refs.filterTable.clearFilter();
+      },
+      formatter(row, column) {
+        return row.address;
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      },
+      filterHandler(value, row, column) {
+        const property = column['property'];
+        return row[property] === value;
+      }
+    }
 }
 </script>
 
 <style>
+  .main {
+  }
+  .announce {
+    margin-top: 0px;
+    width: 100%;
+    height: 600px;
+  }
+  .ann_title{
+    background-color:#D1D0AA;
+    margin-top: 0; 
+    height: 140px;
+  }
+  .ann_body {
+    height: 100%;
+    padding: 10px 20px 10px 20px;
+  }
+  .ann_body_list {
+    margin: 20px;
+    height: 300px;
+    padding:10px;
+    border: 2px solid;
+    border-color: black;
+    border-radius: 10px;
+  }
 
+  .activity {
+    margin-top: 0px;
+    width: 100%;
+    height: 600px;
+  }
+  .act_title{
+    background-color:#D1D0AA;
+    margin-top: 0; 
+    height: 140px;
+  }
+  .act_body {
+    height: 100%;
+    padding: 10px 20px 10px 20px;
+  }
+  .act_body_list {
+    margin: 20px;
+    height: 300px;
+    padding:10px;
+    border: 2px solid;
+    border-color: black;
+    border-radius: 10px;
+  }
 </style>
